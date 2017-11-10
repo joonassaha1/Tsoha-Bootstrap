a4177d9a38db72451a7ac25ce19664cef501795b
Joonas Saha <joonassaha@lpf0rc521.acc.master.epnet>
2017-11-10 16:59:55 +0200

    tokan viikon jutut

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,11 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+
+INSERT INTO Chef (name, password) VALUES ('Ekakokki', '1234');
+INSERT INTO Chef (name, password) VALUES ('Tokakokki', '1234');
+
+INSERT INTO Foodstuff (name, description) VALUES ('Makarooni', 'Makarooni on pastaa');
+INSERT INTO Foodstuff (name, description) VALUES ('Laatikko', 'Laatikko on puuta');
+
+INSERT INTO Recipe (name, description, date) VALUES ('Makaroonilaatikko', 'Ei kovin hyvää', '2017-11-10');
+
+INSERT INTO Ingredient (name) VALUES ('Suola');
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,29 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+
+CREATE TABLE Chef(
+    id SERIAL PRIMARY KEY,
+    name varchar(50) NOT NULL,
+    password varchar(50) NOT NULL
+);
+
+CREATE TABLE Foodstuff(
+    id SERIAL PRIMARY KEY
+    name varchar(50) NOT NULL,
+    description varchar(500),
+);
+
+CREATE TABLE Recipe(
+    chef_id INTEGER REFERENCES Chef(id),
+    foodstuff_id INTEGER REFERENCES Foodstuff(id),
+    name varchar(50) NOT NULL,
+    description varchar(500),
+    status boolean DEFAULT TRUE,
+    published DATE
+);
+
+CREATE TABLE Ingredient(
+    foodstuff_id INTEGER REFERENCES Foodstuff(id),
+    name varchar(50) NOT NULL
+);
+
+
